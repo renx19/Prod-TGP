e8ee6f8b69c48ee9b3f5978f01100a5e567052f3
renx19 <1markjustinborja1@gmail.com>
2024-11-09 00:25:44 +0800

    Update server.js

--- TGPx/Backend/server.js
+++ TGPx/Backend/server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://prod-tgp.vercel.app', '*'], // Add the Vercel URL here
+    origin: ['http://localhost:5173', 'https://prod-tgp.vercel.app', ], // Add the Vercel URL here
     credentials: true
 }));
 
